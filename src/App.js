b4503822d6f6b864ad06901a4e145dc4bf6951dc
GANESH BABU <jgharish12a@gmail.com>
2023-11-08 12:31:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [isVisible, setIsVisible] = useState(false);
+  const [backgroundColor, setBackgroundColor] = useState("aquamarine");
+
+  const toggleVisibility = () => {
+    setIsVisible(!isVisible);
+  };
+
+  const changeBackgroundColor = (color) => {
+    setBackgroundColor(color);
+    console.log(color + " button pressed");
+  };
+  
+
+  const appStyle = {
+    backgroundColor: backgroundColor,
+    transition: "background-color 0.5s", // Optional smooth transition
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="outerdiv" style={appStyle}>
+      <div className="button1">
+        <button onClick={toggleVisibility}>Pick a Color</button>
+      </div>
+
+      {isVisible && (
+        <div className="arrayofcolors">
+          <button
+            onClick={() => changeBackgroundColor("blue")}
+            style={{ backgroundColor: "blue" }}
+          >
+            Blue
+          </button>
+          <button
+            onClick={() => changeBackgroundColor("red")}
+            style={{ backgroundColor: "red" }}
+          >
+            Red
+          </button>
+          <button
+            onClick={() => changeBackgroundColor("yellow")}
+            style={{ backgroundColor: "yellow" }}
+          >
+            Yellow
+          </button>
+          <button
+            onClick={() => changeBackgroundColor("green")}
+            style={{ backgroundColor: "green" }}
+          >
+            Green
+          </button>
+          <button
+            onClick={() => changeBackgroundColor("pink")}
+            style={{ backgroundColor: "pink" }}
+          >
+            Pink
+          </button>
+        </div>
+      )}
     </div>
   );
 }
